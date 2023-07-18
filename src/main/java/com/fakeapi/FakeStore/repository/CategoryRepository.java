b34d8ac6419bc2405d87d9cf73322d1afadcfa1f
HJC96 <gkswlcjs2@naver.com>
2023-07-19 06:37:09 +0900

    Add Function - Login with(JWT)

--- src/main/java/com/fakeapi/FakeStore/repository/CategoryRepository.java
+++ src/main/java/com/fakeapi/FakeStore/repository/CategoryRepository.java
@@ -2,7 +2,9 @@ package com.fakeapi.FakeStore.repository;
 
 import com.fakeapi.FakeStore.domain.Category;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface CategoryRepository extends JpaRepository<Category, Long> {
 }
 
