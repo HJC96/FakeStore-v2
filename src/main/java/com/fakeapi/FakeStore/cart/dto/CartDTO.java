57c90ccd05211cdc984376b7cba3888bd93f33fc
HJC96 <gkswlcjs2@naver.com>
2025-05-11 10:38:21 +0900

    feat: Cart 리팩토링(JPAQueryFactory 사용)

--- src/main/java/com/fakeapi/FakeStore/cart/dto/CartDTO.java
+++ src/main/java/com/fakeapi/FakeStore/cart/dto/CartDTO.java
@@ -1,22 +1,22 @@
 package com.fakeapi.FakeStore.cart.dto;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
-import lombok.AllArgsConstructor;
 import lombok.Builder;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
 
 import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 
-@Data @Builder
-@AllArgsConstructor
-@NoArgsConstructor
+@Builder
+@Getter
+@Setter
 public class CartDTO {
 
     private Long id;
     private Long userId;
+
     @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss.SSS'Z'")
     private LocalDateTime date;
     private List<CartItemDTO> products = new ArrayList<>();
