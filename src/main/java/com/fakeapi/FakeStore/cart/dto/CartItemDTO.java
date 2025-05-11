678d6dcd6bff5542a1d68376a93cebfb794086a7
HJC96 <gkswlcjs2@naver.com>
2025-05-11 21:18:34 +0900

    feat: setter 없어서 data null로 뜨던 현상 수정

--- src/main/java/com/fakeapi/FakeStore/cart/dto/CartItemDTO.java
+++ src/main/java/com/fakeapi/FakeStore/cart/dto/CartItemDTO.java
@@ -4,10 +4,12 @@ import com.fasterxml.jackson.annotation.JsonIgnore;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 @AllArgsConstructor
 @NoArgsConstructor
 @Getter
+@Setter
 public class CartItemDTO {
     @JsonIgnore
     private Long cartId;
